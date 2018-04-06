2d4a03e732dc6b2dff1e73a3c17a08056a1f42af
Yernemm <yernemm@yernemm.xyz>
2018-04-06 01:57:34 +0100

    Added some stuff

---
+++ TwitchEmojiDownloader-NodeJS/twt.js
@@ -0,0 +1,14 @@
+var http = require('https');
+var fs = require('fs');
+
+var j = 1;
+download(j);
+function download(i){
+var file = fs.createWriteStream("emotes/"+i + ".png");
+var request = http.get("https://static-cdn.jtvnw.net/emoticons/v1/"+ i +"/3.0", function(response) {
+  response.pipe(file);
+  console.log(i);
+  j++;
+  download(j);
+});
+}
